0d555f5c25bb113d34895c52074d8c57cd21c157
Michelsgab <eugabmichels@gmail.com>
2022-06-01 17:45:01 -0300

    Input value using useState

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import "./style.css";
 function App() {
   const [counter, setCounter] = useState(0);
   const [subcounter, setSubcounter] = useState(10);
+  const [inputValue, setInputValue] = useState("");
 
   function increment() {
-    setCounter(counter + 1)
+    setCounter(counter + 1);
   }
 
   function decrease() {
-    setSubcounter(subcounter - 1)
+    setSubcounter(subcounter - 1);
+  }
+
+  function changeText(e) {
+    const newValue = e.target.value;
+    setInputValue(newValue);
   }
 
   return (
     <div className="container">
-      <div className="add">
-      <h3>{counter}</h3>
-      <button onClick={increment}> Increment </button>
+      <div className="container-item">
+        <h3>{counter}</h3>
+        <button onClick={increment}> Increment </button>
       </div>
-      <div className="less">
-      <h3>{subcounter}</h3>
-      <button onClick={decrease}> Decrease </button>
+      <div className="container-item">
+        <h3>{subcounter}</h3>
+        <button onClick={decrease}> Decrease </button>
+      </div>
+      <div className="container-input">
+        <input placeholder="enter something..." onChange={changeText} />
+        {inputValue}
       </div>
     </div>
   );
