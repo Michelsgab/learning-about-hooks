57b3707e94139f02c628eaebf6fbafd01339e60c
Michelsgab <eugabmichels@gmail.com>
2022-06-01 17:32:01 -0300

    Learning About useState Hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [subcounter, setSubcounter] = useState(10);
+
+  function increment() {
+    setCounter(counter + 1)
+  }
+
+  function decrease() {
+    setSubcounter(subcounter - 1)
+  }
+
   return (
-    <div>Ola</div>
+    <div className="container">
+      <div className="add">
+      <h3>{counter}</h3>
+      <button onClick={increment}> Increment </button>
+      </div>
+      <div className="less">
+      <h3>{subcounter}</h3>
+      <button onClick={decrease}> Decrease </button>
+      </div>
+    </div>
   );
 }
 
